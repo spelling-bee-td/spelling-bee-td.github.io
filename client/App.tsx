57e8e76b591f477f408082404dabca6395e04122
spelling-bee-td <spelling.bee.td@gmail.com>
2025-11-30 15:22:45 +0800

    Add files via upload

---
+++ client/App.tsx
@@ -0,0 +1,36 @@
+import { Switch, Route, Router as WouterRouter } from "wouter";
+import { useHashLocation } from "wouter/use-hash-location";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import NotFound from "@/pages/not-found";
+import Home from "@/pages/home";
+import PlayPage from "@/pages/play";
+import ChasePage from "@/pages/chase";
+
+function Router() {
+  return (
+    <WouterRouter hook={useHashLocation}>
+      <Switch>
+        <Route path="/" component={Home} />
+        <Route path="/play/:difficulty" component={PlayPage} />
+        <Route path="/chase/:difficulty" component={ChasePage} />
+        <Route component={NotFound} />
+      </Switch>
+    </WouterRouter>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Router />
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
